e0b65da5adfc33a1ffd2be186e86f03743c329de
Yuvanesh V <162546398+YuvaneshV12@users.noreply.github.com>
2025-07-14 15:45:13 +0530

    Update server.js
    Made pool db connection to auto connect and handle failures

--- backend/server.js
+++ backend/server.js
@@ -22,12 +22,15 @@ app.use(cors({
 app.use(express.json());
 
 // MySQL connection using Clever Cloud env variables
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.MYSQL_ADDON_HOST,
   user: process.env.MYSQL_ADDON_USER,
   password: process.env.MYSQL_ADDON_PASSWORD,
   database: process.env.MYSQL_ADDON_DB,
   port: process.env.MYSQL_ADDON_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 db.connect((err) => {
