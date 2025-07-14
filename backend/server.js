fe7431f0223c23b78873a2cdba8fa10254501365
Yuvanesh V <162546398+YuvaneshV12@users.noreply.github.com>
2025-07-14 15:57:22 +0530

    Update server.js
    Connection Undo

--- backend/server.js
+++ backend/server.js
@@ -22,15 +22,12 @@ app.use(cors({
 app.use(express.json());
 
 // MySQL connection using Clever Cloud env variables
-const db = mysql.createPool({
+const db = mysql.createConnection({
   host: process.env.MYSQL_ADDON_HOST,
   user: process.env.MYSQL_ADDON_USER,
   password: process.env.MYSQL_ADDON_PASSWORD,
   database: process.env.MYSQL_ADDON_DB,
   port: process.env.MYSQL_ADDON_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
 });
 
 db.connect((err) => {
